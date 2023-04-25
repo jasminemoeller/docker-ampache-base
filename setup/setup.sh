10bbb500ce078548ededdcac5c61077287316821
Jasper Moeller <jasper.moeller@yworks.com>
2023-04-25 13:56:58 +0200

    Update to 5.6.0

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,10 +6,10 @@ set -x
 cd /tmp/setup
 
 # WHEN UPDATING VERSION, ALSO UPDATE GITLAB CI FILE!
-VERSION=5.5.5
+VERSION=5.6.0
 
 wget https://github.com/ampache/ampache/releases/download/$VERSION/ampache-${VERSION}_all.zip
-sha256sum -c < sums
+md5sum -c < sums
 
 cd /var/www/html
 mkdir ampache
